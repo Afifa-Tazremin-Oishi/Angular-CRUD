2e7c2f3a2f06407a452030d1bf36a43bd5f53739
Afifa-Tazremin-Oishi <afifatazreminoishi@gmail.com>
2022-09-17 03:29:55 +0600

    update

--- src/app/employee-dashboard/employee-dashboard.component.ts
+++ src/app/employee-dashboard/employee-dashboard.component.ts
@@ -1,6 +1,6 @@
 import { ApiService } from './../shared/api.service';
 //import { EmployeeModel } from './employee-dashboard.model';
-import { Component, OnInit } from '@angular/core';
+import { assertPlatform, Component, OnInit } from '@angular/core';
 import {FormBuilder,FormGroup} from '@angular/forms';
 import { EmployeeModel } from './models/employee-dashboard.model'
 
@@ -13,6 +13,7 @@ export class EmployeeDashboardComponent implements OnInit {
 
   formValue !: FormGroup;
   employeeModelObj : EmployeeModel = new EmployeeModel();
+  employeeData !: any;
   //api: any;
   constructor(private formbuilder: FormBuilder, private api : ApiService){ }
 
@@ -22,9 +23,10 @@ export class EmployeeDashboardComponent implements OnInit {
      lastName: [''],
      email: [''],
      mobile: [''],
-     salary: ['']
+     salary: [''],
   
   })
+  this.getAllEmployee();
   }
   postEmployeeDetails(){
     this.employeeModelObj.firstName = this.formValue.value.firstName;
@@ -36,12 +38,57 @@ export class EmployeeDashboardComponent implements OnInit {
     this.api.postEmployee(this.employeeModelObj).subscribe (res=>{
       console.log(res);
       alert("Employee Added Successfully");
+      let ref = document.getElementById('cancle')
+      ref?.click();
+      this.formValue.reset();
+      this.getAllEmployee();
     },
     err=>{
         alert("Something Went Wrong");
        }
      )
       }
+
+      getAllEmployee(){
+        this.api.getEmployee().subscribe(res=>{
+          this.employeeData = res;
+        })
+      }
+
+      deleteEmployee(row : any){
+        this.api.deleteEmployee(row.id).subscribe(res=>{
+          alert("Employee Deleted");
+          this.getAllEmployee();
+        })
+      }
+
+      onEdit(row : any){
+        this.employeeModelObj.id = row.id;
+        this.formValue.controls['firstName'].setValue(row.firstName);
+        this.formValue.controls['lastName'].setValue(row.lastName);
+        this.formValue.controls['email'].setValue(row.email);
+        this.formValue.controls['mobile'].setValue(row.mobile);
+        this.formValue.controls['salary'].setValue(row.salary);
+      }
+
+      updateEmployeeDetails(){
+        this.employeeModelObj.firstName = this.formValue.value.firstName;
+        this.employeeModelObj.lastName = this.formValue.value.lastName;
+        this.employeeModelObj.email = this.formValue.value.email;
+        this.employeeModelObj.mobile = this.formValue.value.mobile;
+        this.employeeModelObj.salary = this.formValue.value.salary;
+
+        this.api.updateEmployee(this.employeeModelObj , this.employeeModelObj.id).subscribe(res=>{
+          alert("Update Successfully");
+          
+          let ref = document.getElementById('cancle')
+          ref?.click();
+          this.formValue.reset();
+          this.getAllEmployee();
+        })
+      }
+
+
 
     // .subscribe(res=>{
     //   console.log(res);
--- src/app/shared/api.service.ts
+++ src/app/shared/api.service.ts
@@ -11,7 +11,25 @@ export class ApiService {
   constructor(private http : HttpClient) { }
 
   postEmployee(data : any){
-    return this.http.post<any>('http://localhost:3000/posts', data)
+    return this.http.post<any>('http://localhost:3000/posts/', data)
+    .pipe(map((res:any)=>{
+      return res;
+    }))
+  }
+  getEmployee(){
+    return this.http.get<any>('http://localhost:3000/posts/')
+    .pipe(map((res:any)=>{
+      return res;
+    }))
+  }
+  deleteEmployee(id : number){
+  return this.http.delete<any>('http://localhost:3000/posts/' + id)
+    .pipe(map((res:any)=>{
+      return res;
+    }))
+  }
+  updateEmployee(id : number, data : any){
+    return this.http.put<any>('http://localhost:3000/posts/' + id , data)
     .pipe(map((res:any)=>{
       return res;
     }))
